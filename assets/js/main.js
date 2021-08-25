5ff03f036b9da74078539424eb6697d685701f2b
Anand Sunderrajan <36977554+Xeno-404@users.noreply.github.com>
2021-08-24 22:24:37 -0600

    Add the nav menu function

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*===== Function to show the menu on navigation bar for smaller screen =====*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
