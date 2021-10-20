fd6cbf0ce0dcd60cde5f724d13f2b268a8eabfb4
Anand Sunderrajan <36977554+Xeno-404@users.noreply.github.com>
2021-10-19 23:32:41 -0500

    Sizing and display fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@ const loader = new GLTFLoader()
 loader.load('assets/img/final.glb', function(glb){
     console.log(glb)
     const root = glb.scene;
-    root.scale.set(1.2,1,1)
+    // root.scale.set(1.2,1,1)
     scene.add(root);
 }, function(xhr){
     console.log((xhr.loaded/xhr.total * 100) + "% loaded")
@@ -23,7 +23,7 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(100, sizes.width/sizes.height, 0.1, 100)
-const light = new THREE.DirectionalLight(0xffffff)
+const light = new THREE.AmbientLight(0xffffff)
 light.position.set(0,0,0)
 camera.add(light)
 
@@ -36,6 +36,8 @@ const controls = new OrbitControls( camera, renderer.domElement );
 camera.position.set(1,1,4);
 // camera.translateY(10)
 controls.update();
+controls.autoRotate = true
+controls.autoRotateSpeed = 3.0
 scene.add(camera)
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(canvas.devicePixelRatio)
