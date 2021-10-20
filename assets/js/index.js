ac861091a295cd7048bf97ffb77f2886797ffcfa
Anand Sunderrajan <36977554+Xeno-404@users.noreply.github.com>
2021-10-19 22:29:51 -0500

    3D model and three.js code

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+import * as THREE from '/three.js-master/build/three.module.js'
+import {GLTFLoader} from '/three.js-master/examples/jsm/loaders/GLTFLoader.js'
+import {OrbitControls} from '/three.js-master/examples/jsm/controls/OrbitControls.js'
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+// console.log(THREE)
+
+const loader = new GLTFLoader()
+loader.load('assets/img/final.glb', function(glb){
+    console.log(glb)
+    const root = glb.scene;
+    root.scale.set(1.2,1,1)
+    scene.add(root);
+}, function(xhr){
+    console.log((xhr.loaded/xhr.total * 100) + "% loaded")
+}, function(error){
+    console.log('error happen :(')
+}
+)
+const sizes = {
+    width: innerWidth/2,
+    height: innerHeight/1.5
+}
+
+const camera = new THREE.PerspectiveCamera(100, sizes.width/sizes.height, 0.1, 100)
+const light = new THREE.DirectionalLight(0xffffff)
+light.position.set(0,0,0)
+camera.add(light)
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas:canvas,
+    alpha:true
+})
+
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.set(1,1,4);
+// camera.translateY(10)
+controls.update();
+scene.add(camera)
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(canvas.devicePixelRatio)
+renderer.shadowMap.enabled = true
+
+function animate() {
+    requestAnimationFrame(animate)
+    controls.update()
+    camera.add(light)
+    renderer.render(scene,camera)
+}
+
+animate()
+
+
+
