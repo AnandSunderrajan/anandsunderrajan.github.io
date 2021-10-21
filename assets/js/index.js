b6021517dad4376255f0418ae82469907e3cdb0b
Anand Sunderrajan <36977554+Xeno-404@users.noreply.github.com>
2021-10-21 17:56:35 -0500

    3d model sizing fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,7 +19,7 @@ loader.load('assets/img/final.glb', function(glb){
 )
 const sizes = {
     width: innerWidth/2,
-    height: innerHeight/1.25
+    height: innerHeight/2
 }
 
 const camera = new THREE.PerspectiveCamera(100, sizes.width/sizes.height, 0.1, 100)
